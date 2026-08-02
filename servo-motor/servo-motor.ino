a3b50ea10f20beeb479faaf31fe0403ff3acbc90
codingwithrahat <codingwithrahat@gmail.com>
2026-08-03 01:53:17 +0600

    servo

---
+++ servo-motor/servo-motor.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+
+Servo servo;
+
+void setup() {
+  servo.attach(6);
+
+}
+
+void loop() {
+
+  servo.write(0);
+  delay(2000);
+
+  servo.write(90);
+  delay(2000);
+
+
+  servo.write(180);
+  delay(2000);
+
+  servo.write(90);
+  delay(2000);
+
+}
