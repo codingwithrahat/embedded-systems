0db9d18348b54502f748eed59bcc1e3f034165b7
codingwithrahat <codingwithrahat@gmail.com>
2026-08-04 01:03:13 +0600

    led

--- led-blink/led-blink.ino
+++ led-blink/led-blink.ino
@@ -1,40 +1,23 @@
-int trigPin = 10;
-int echoPin = 8;
-int led = 13;
-int buzzer = 12;
+int pin = 13;
+int pin2 = 12;
 
-void setup() {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(led, OUTPUT);
-  pinMode(buzzer, OUTPUT);
-
-  Serial.begin(9600);
-
+void setup()
+{
+    pinMode(pin, OUTPUT);
+    pinMode(pin2, OUTPUT);
 }
 
-void loop() {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
+void loop()
+{
+    digitalWrite(pin, HIGH);
+    delay(100);
 
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
+    digitalWrite(pin, LOW);
+    delay(100);
 
-  digitalWrite(trigPin, LOW);
+    digitalWrite(pin2, HIGH);
+    delay(100);
 
-  long time = pulseIn(echoPin, HIGH, 30000);
-  float distance = (0.0344 * time) / 2;
-
-  Serial.println(distance);
-
-  if(distance <= 100){
-    digitalWrite(led, HIGH);
-    digitalWrite(buzzer, HIGH);
-  }else{
-    digitalWrite(led, LOW);
-    digitalWrite(buzzer, LOW);
-  }
-  
-  delay(500);
-
+    digitalWrite(pin2, LOW);
+    delay(100);
 }
